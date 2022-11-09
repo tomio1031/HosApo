a08060a8493c34766df5a9fab35a415e1f4350b4
muramasa08 <nomukinn0815@gmail.com>
2022-11-09 10:11:28 +0900

    Update apo.php

--- apo.php
+++ apo.php
@@ -1,4 +1,8 @@
-<img src="./img/1_Primary_logo_on_transparent_267x69.png" onclick="location.href='./top.html'">
+<?php
+$filePath ='./img/1_Primary_logo_on_transparent_267x69.png'
+$data=file_get_contents($filePath);
+header('Content-type: img/png');
+echo $data;
     <!-- 病院名を表示 -->
     <?php 
     ?>

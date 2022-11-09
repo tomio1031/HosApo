d83e2d488db1b3431d84b3aa0f0a5523a0026821
muramasa08 <nomukinn0815@gmail.com>
2022-11-09 10:36:28 +0900

    Update apo.php

--- apo.php
+++ apo.php
@@ -3,8 +3,10 @@ $filePath ='./img/1_Primary_logo_on_transparent_267x69.png'
 $data=file_get_contents($filePath);
 header('Content-type: img/png');
 echo $data;
+?>
     <!-- 病院名を表示 -->
     <?php 
+    $name="mysql:host=db;dbname=dbname;charset=uft-8"
     ?>
     <h2>病院予約</h2>
     <p>診療科を選択してください</p>

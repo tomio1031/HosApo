b470bb18bb1fe6c489b404bc2c5e73e9d9b80471
muramasa08 <nomukinn0815@gmail.com>
2022-11-11 13:58:19 +0900

    git add h2

--- apo.php
+++ apo.php
@@ -1,9 +1,4 @@
-<?php
-$filePath ='./img/1_Primary_logo_on_transparent_267x69.png'
-$data=file_get_contents($filePath);
-header('Content-type: img/png');
-echo $data;
-?>
+
     <!-- 病院名を表示 -->
     <?php 
     $name="mysql:host=db;dbname=dbname;charset=uft-8"

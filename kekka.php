d553f2e161195c965d76efb5c70e2227fc14f576
Hayayayato <toyoyon1214@gmail.com>
2022-11-11 15:01:02 +0900

    Update kekka.php

---
+++ kekka.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="css/hos_info.css">
+    <title>Document</title>
+</head>
+
+<!-- ヘッダー -->
+<header>
+        <h1 class="rogo">
+            <a href="">
+                <img src="img/1_Primary_logo_on_transparent_267x69.png">
+            </a>
+        </h1>
+        <button class="btn btn-outline-success btn-lg" id="rogout">ログアウト</button>
+    </header>
+
+    <!--戻るボタン -->
+    <a href="top.php">戻る</a>
+
+    
+<body>
+</body>
+</html>

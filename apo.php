1fa9ae28c3203941e1ed278b8c79e6bcf5c40952
muramasa08 <nomukinn0815@gmail.com>
2022-11-14 09:40:04 +0900

    Update apo.php

--- apo.php
+++ apo.php
@@ -7,7 +7,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-        <link href="https: //fonts.googleapis.com/css2? family= Noto+Sans+JP & display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
         <style>
             .container{
                 font-family: 'Noto Sans JP', sans-serif;

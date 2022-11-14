54efd3db473940a80f70b1048aff04ea62314bfe
muramasa08 <nomukinn0815@gmail.com>
2022-11-14 10:33:29 +0900

    Update apo_1.php

--- apo_1.php
+++ apo_1.php
@@ -90,7 +90,7 @@ for ( $day = 1; $day <= $day_count; $day++, $youbi++) {
                     <img src="img/1_Primary_logo_on_transparent_267x69.png">
                 </a>
             </h1>
-            <button class="btn btn-outline-success btn-lg" id="rogout">ログアウト</button>
+            <button href="login.html" class="logout" id="rogout">ログアウト</button>
         </header>
         <div class="container">
         <h2>予約したい日時を選択してください</h2>

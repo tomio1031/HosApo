edbd724defad486e4003a75976b5325df203b3a6
muramasa08 <nomukinn0815@gmail.com>
2022-11-14 10:02:50 +0900

    Update apo.php

--- apo.php
+++ apo.php
@@ -1,4 +1,47 @@
+<?php
+date_default_timezone_set('Asia/Tokyo');
 
+if(isset($_GET['ym'])){
+    $ym=$_GET['ym'];
+}else{
+    $ym=date('Y-m');
+}
+$timestamp=strtotime($ym . '-01');
+if($timestamp === false) {
+    $ym = date('Y-m');
+    $timestamp = strtotime($ym . '-01');
+}
+$today = date('Y-m-j');
+$html_title = date('Y年n月', $timestamp);
+$prev = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)-1, 1, date('Y', $timestamp)));
+$next = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)+1, 1, date('Y', $timestamp)));
+$day_count = date('t', $timestamp);
+$youbi = date('w', mktime(0, 0, 0, date('m', $timestamp), 1, date('Y', $timestamp)));
+$weeks = [];
+$week = '';
+$week .= str_repeat('<td></td>', $youbi);
+
+for ( $day = 1; $day <= $day_count; $day++, $youbi++) {
+
+    $date = $ym . '-' . $day;
+
+    if ($today == $date) {
+        $week .= '<td class="today">' . $day;
+    } else {
+        $week .= '<td>' . $day;
+    }
+    $week .= '</td>';
+    if ($youbi % 7 == 6 || $day == $day_count) {
+
+        if ($day == $day_count) {
+            $week .= str_repeat('<td></td>', 6 - $youbi % 7);
+        }
+        $weeks[] = '<tr>' . $week . '</tr>';
+
+        $week = '';
+    }
+}
+?>
     <!DOCTYPE html>
     <html lang="ja">
     <head>
@@ -95,6 +138,11 @@
                     <td></td>
                     <td></td>
                 </tr>
+                <?php
+                foreach ($weeks as $week) {
+                    echo $week;
+                }
+            ?>
             </table>
         </div>
     </body>

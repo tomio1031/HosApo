41a3e3a6ea79f8094f4e53f399ede5fabc7390e8
muramasa08 <nomukinn0815@gmail.com>
2022-11-14 10:04:21 +0900

    Update apo.php

--- apo.php
+++ apo.php
@@ -93,51 +93,6 @@ for ( $day = 1; $day <= $day_count; $day++, $youbi++) {
                     <th>金</th>
                     <th>土</th>
                 </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>3</td>
-                    <td>4</td>
-                    <td>5</td>
-                </tr>
-                <tr>
-                    <td>6</td>
-                    <td>7</td>
-                    <td>8</td>
-                    <td>9</td>
-                    <td>10</td>
-                    <td class="today">11</td>
-                    <td>12</td>
-                </tr>
-                <tr>
-                    <td>13</td>
-                    <td>14</td>
-                    <td>15</td>
-                    <td>16</td>
-                    <td>17</td>
-                    <td>18</td>
-                    <td>19</td>
-                </tr>
-                <tr>
-                    <td>20</td>
-                    <td>21</td>
-                    <td>22</td>
-                    <td>23</td>
-                    <td>24</td>
-                    <td>25</td>
-                    <td>26</td>
-                </tr>
-                <tr>
-                    <td>27</td>
-                    <td>28</td>
-                    <td>29</td>
-                    <td>30</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
                 <?php
                 foreach ($weeks as $week) {
                     echo $week;

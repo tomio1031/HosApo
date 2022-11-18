0dadeb860707b64c1d095c7c27d854f8b4bfbf27
muramasa08 <nomukinn0815@gmail.com>
2022-11-18 12:40:19 +0900

    Update apo_1.php

--- apo_1.php
+++ apo_1.php
@@ -25,9 +25,9 @@ for ( $day = 1; $day <= $day_count; $day++, $youbi++) {
 
     $date = $ym . '-' . $day;
 
-    $link = '<a href="menupost.php?date="' . $date . '>' . $day . '</a>'
+    $link = '<a href="menupost.php?date="' . $date . '>' . $day . '</a>';
     if ($today == $date) {
-        $week .= '<td class="today">' . $link .'</a>';
+        $week .= '<td class="today">' . $link .'</td>';
     } else {
         $week .= '<td>' . $link . '</td>';
     }

38dc4e5de85c19e5033d7d531b3dc09e056839ba
owoodchucko <amu00934x0405@gmail.com>
2022-12-07 12:13:28 +0900

    aaa

--- DBmanager1.php
+++ DBmanager1.php
@@ -8,98 +8,17 @@ class DBManager{
     }
 
 
-    //取得　病院ID
-    public function get_hospital_id($kensaku_word){
+    //取得　ユーザーテーブルのIDとパスワード
+    public function getUserTblByIdAndPass($hospital_id){
       $pdo = $this->dbConnect();
-      $sql = "SELECT * FROM hosapo_hospital_tbl WHERE hospital_name LIKE ?";
+      $sql = "SELECT * FROM hosapo_hospital_tbl WHERE hospital_id = ? ";
       $ps = $pdo->prepare($sql);
-      $ps->bindValue(1, "%" . $kensaku_word . "%", PDO::PARAM_STR);
+      $ps->bindValue(1, $hospital_id, PDO::PARAM_INT);
       $ps->execute();
       $searchArray = $ps->fetchAll();
       return $searchArray;
+  
       }
-
-    //取得　診療科
-    public function get_medical_specialty($medical_specialty){
-      $pdo = $this->dbConnect();
-      $sql = "SELECT * FROM hosapo_hospital_tbl WHERE medical_specialty LIKE ?";
-      $ps = $pdo->prepare($sql);
-      $ps->bindValue(1, "%" . $medical_specialty . "%", PDO::PARAM_STR);
-      $ps->execute();
-      $searchArray = $ps->fetchAll();
-      return $searchArray;
-      }
-
-
-    //取得　エリア
-    public function get_hospital_area($hospital_area){
-      $pdo = $this->dbConnect();
-      $sql = "SELECT * FROM hosapo_hospital_tbl WHERE hospital_area LIKE ?";
-      $ps = $pdo->prepare($sql);
-      $ps->bindValue(1, "%" . $hospital_area . "%", PDO::PARAM_STR);
-      $ps->execute();
-      $searchArray = $ps->fetchAll();
-      return $searchArray;
-    }
-
-
-    //取得　病院名
-    public function get_hospital_name($hospital_name){
-      $pdo = $this->dbConnect();
-      $sql = "SELECT * FROM hosapo_hospital_tbl WHERE hospital_name LIKE ?";
-      $ps = $pdo->prepare($sql);
-      $ps->bindValue(1, "%" . $hospital_name . "%", PDO::PARAM_STR);
-      $ps->execute();
-      $searchArray = $ps->fetchAll();
-      return $searchArray;
-    }
-
-
-    //取得　電話番号
-    public function get_hospital_phone_number($hospital_phone_number){
-      $pdo = $this->dbConnect();
-      $sql = "SELECT * FROM hosapo_hospital_tbl WHERE hospital_phone_number LIKE ?";
-      $ps = $pdo->prepare($sql);
-      $ps->bindValue(1, "%" . $hospital_phone_number . "%", PDO::PARAM_STR);
-      $ps->execute();
-      $searchArray = $ps->fetchAll();
-      return $searchArray;
-    }
-
-
-    //取得　住所
-    public function get_hospital_address($hospital_address){
-      $pdo = $this->dbConnect();
-      $sql = "SELECT * FROM hosapo_hospital_tbl WHERE hospital_address LIKE ?";
-      $ps = $pdo->prepare($sql);
-      $ps->bindValue(1, "%" . $hospital_address . "%", PDO::PARAM_STR);
-      $ps->execute();
-      $searchArray = $ps->fetchAll();
-      return $searchArray;
-    }
-
-
-    //取得　交通アクセス
-    public function get_traffic_access($traffic_access){
-      $pdo = $this->dbConnect();
-      $sql = "SELECT * FROM hosapo_hospital_tbl WHERE traffic_access LIKE ?";
-      $ps = $pdo->prepare($sql);
-      $ps->bindValue(1, "%" . $traffic_access . "%", PDO::PARAM_STR);
-      $ps->execute();
-      $searchArray = $ps->fetchAll();
-      return $searchArray;
-    }
-
-    //取得　写真
-    public function get_hospital_picture($hospital_picture){
-      $pdo = $this->dbConnect();
-      $sql = "SELECT * FROM hosapo_hospital_tbl WHERE hospital_picture LIKE ?";
-      $ps = $pdo->prepare($sql);
-      $ps->bindValue(1, "%" . $hospital_picture . "%", PDO::PARAM_STR);
-      $ps->execute();
-      $searchArray = $ps->fetchAll();
-      return $searchArray;
-    }
 
                       //取得　コメント
     public function getcomment($hospital_introduction){

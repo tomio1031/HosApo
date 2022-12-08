6201e62ffa470b04ea8e05369c33572a241af2c0
Hayayayato <toyoyon1214@gmail.com>
2022-12-08 13:44:36 +0900

    aaa

--- kekka.php
+++ kekka.php
@@ -25,13 +25,13 @@
     </div>
 
 
+<div class = "kensakukekka">
 <?php
 
 require_once 'DBManager.php';
 $dbmng = new DBManager();
 $userList = $dbmng->getUserTblByid($_POST['kensaku-word'],$_POST['hospital_name'],$_POST['hospital_address'],$_POST['hospital_phone_number'],$_POST['medical_specialty']);
-echo "<div class = 'kensakukekka'><h1>検索ワード:". $_POST['kensaku-word']."　　　　　　検索結果：".count($userList)."件</h1>";//フリーワードと件数を表示
-echo "<\div>"
+echo "<h1>検索ワード:". $_POST['kensaku-word']."　　　　　　検索結果：".count($userList)."件</h1>";//フリーワードと件数を表示
     foreach($userList as $row){
         echo "<div> <img src='./img/24636406_s.jpg'>";
         echo "$row[hospital_name]<br>";
@@ -45,6 +45,7 @@ echo "<\div>"
 
 ?>
 
+</div>
 
 
 </body>
